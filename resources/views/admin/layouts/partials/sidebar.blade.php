20c80ba1ab77dcf1b5edff9aaa00cbf99ff9fd4a
Abdussalam Al-Ali <abdussalam.alali95@gmail.com>
2020-08-03 14:55:28 +0300

    init1

--- resources/views/admin/layouts/partials/sidebar.blade.php
+++ resources/views/admin/layouts/partials/sidebar.blade.php
@@ -6,7 +6,7 @@
         <li><a href="{{route('users.indexmanager')}}">المشرفون <i class="fa fa-angle-double-left pull-left"></i></a></li>
         <li><a href="{{route('users.indexteacher')}}">المعلمون<i class="fa fa-angle-double-left pull-left"></i></a></li>
         <li><a href="{{route('users.indexstudent')}}"> الطلاب<i class="fa fa-angle-double-left pull-left"></i></a></li>
-        <li><a href="{{route('class.index')}}">التخصصات والصفوف<i class="fa fa-angle-double-left pull-left"></i></a></li>
+        <li><a href="{{route('class.index')}}">البرامج الدرسية<i class="fa fa-angle-double-left pull-left"></i></a></li>
         <li><a href="{{route('course.index') }}">الدورات<i class="fa fa-angle-double-left pull-left"></i></a></li>
         <li><a href="{{route('subject.index')}}">المواد<i class="fa fa-angle-double-left pull-left"></i></a></li>
         <li><a href="{{route('unit.index')}}">الوحدات الدرسية<i class="fa fa-angle-double-left pull-left"></i></a></li>
@@ -16,13 +16,13 @@
         <li><a href="{{route('test.index')}}">الأختبارات<i class="fa fa-angle-double-left pull-left"></i></a></li>
         <li><a href="{{route('attachment.index')}}">المرفقات<i class="fa fa-angle-double-left pull-left"></i></a></li>
 
-        <li><a href="{{route('notes.index')}}">الملاحظات<i class="fa fa-angle-double-left pull-left"></i></a></li>
+        <li><a href="{{route('notes.index')}}">الاعلانات<i class="fa fa-angle-double-left pull-left"></i></a></li>
 
         @endif
         @if (Auth::user()->hasRole(1))
         <li><a href="{{route('users.indexteacher')}}">المعلمون<i class="fa fa-angle-double-left pull-left"></i></a></li>
         <li><a href="{{route('users.indexstudent')}}"> الطلاب<i class="fa fa-angle-double-left pull-left"></i></a></li>
-        <li><a href="{{route('class.index')}}">الصفوف<i class="fa fa-angle-double-left pull-left"></i></a></li>
+        <li><a href="{{route('class.index')}}">البرامج الدرسية<i class="fa fa-angle-double-left pull-left"></i></a></li>
         <li><a href="{{route('course.index') }}">الدورات<i class="fa fa-angle-double-left pull-left"></i></a></li>
         <li><a href="{{route('subject.index')}}">المواد<i class="fa fa-angle-double-left pull-left"></i></a></li>
         <li><a href="{{route('unit.index')}}">الوحدات الدرسية<i class="fa fa-angle-double-left pull-left"></i></a></li>
@@ -32,7 +32,7 @@
         <li><a href="{{route('test.index')}}">الأختبارات<i class="fa fa-angle-double-left pull-left"></i></a></li>
         <li><a href="{{route('attachment.index')}}">المرفقات<i class="fa fa-angle-double-left pull-left"></i></a></li>
 
-        <li><a href="{{route('notes.index')}}">الملاحظات<i class="fa fa-angle-double-left pull-left"></i></a></li>
+        <li><a href="{{route('notes.index')}}">الاعلانات<i class="fa fa-angle-double-left pull-left"></i></a></li>
         @endif
         @if (Auth::user()->hasRole(2))
         <li><a href="{{route('subject.index')}}">المواد<i class="fa fa-angle-double-left pull-left"></i></a></li>
