d9821fc15a9d50e75afdad3558830d83f518018e
Abdussalam Al-Ali <abdussalam.alali95@gmail.com>
2020-08-20 15:19:16 +0300

    clear cache

--- storage/framework/views/1734ef4a58fc2c80618d0b18f16fd21bf9f2fe8e.php
+++
@@ -1,7 +0,0 @@
-<h3>
-    عزيزي الطالب <?php echo e($name); ?>
-
-</h3>
-
-<h4>تم تحديد موعد محاضرة مباشرة في مادة <?php echo e($subject); ?>  في الموعد <?php echo e($date); ?></h4>
-<?php /**PATH C:\xamppp\htdocs\laravel5.8\blog\resources\views/mail/dates.blade.php ENDPATH**/ ?>
